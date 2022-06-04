22db9f39dcdb05647d2a7110077af1f2504ca7f4
zahidul76 <zahidul.islam20152015@gmail.com>
2022-06-04 14:38:18 -0500

    Initial Commit

---
+++ src/main/java/VariableDataTypes.java
@@ -0,0 +1,71 @@
+public class VariableDataTypes {
+    public static void main(String[] args) {
+        byte firstNumber;
+        firstNumber = 32;
+        System.out.println(firstNumber);
+
+        byte secondNumber;
+        secondNumber = 14;
+
+        System.out.println(secondNumber);
+
+        System.out.println(firstNumber);
+
+        byte age;
+        age=45;
+        System.out.println(age);
+        age=120;
+        System.out.println("My age is " + age); // + sign is concat sign which called concatenation
+
+        short age1;
+        age1 = 13;
+        System.out.println(age1);
+
+        int emId;
+        emId=15000;
+        System.out.println("Employee id is "+ emId);
+
+        int areaOfUsa;
+        areaOfUsa =  9000000;
+        System.out.println("Area of USA "  + areaOfUsa);
+
+        long distanceOfSun;
+        distanceOfSun = 20000000000000000L;
+        System.out.println("distance of sun " + distanceOfSun + " KM");
+
+
+        float salary;
+        salary = 95000.50F + 50F ;
+        System.out.println("salary is " + salary);
+
+
+        double budget;
+        budget= 30000000000000000.99;
+        System.out.println("budget of BD "+ budget);
+
+
+
+        //Store Gender in variable
+        char gender;
+        gender= 'M';
+        System.out.println("Gender is"+gender);
+
+
+        //Store is Paid information status in variable
+
+        boolean isPaid;
+        isPaid = true;
+        System.out.println("IsPaid is "+isPaid);
+
+
+
+        byte age11;
+        age11=90;
+        System.out.println("Shohag age is "+ age11);
+
+
+
+
+    }
+
+}
